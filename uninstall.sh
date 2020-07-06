5ecce67b61be5036bd7785ea0f9e3b8caa4f9bd4
Julian Lehrer <jmlehrer@ucsc.edu>
2020-07-06 08:54:32 -0700

    Updated uninstall script to handle the user deleting the backup folder in ~/

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,29 @@
 if [ ! -f "${HOME}/.touchid-for-sudo/sudo" ]; then 
 	echo "The ~/.touchid-for-sudo folder has been deleted, and the original
 /etc/pam.d/sudo file has been lost. Would you like to restore from the remote
-file found on https://github.com/jlehrer1/touchid-for-sudo? Note that it may not
-be the same and could lead to problems. (y/n) "
-	
+file found on https://github.com/jlehrer1/touchid-for-sudo? A copy of the
+current /etc/pam.d/sudo will be stored in ~/.touchid-for-sudo-remote-restore. Note that this
+change may lead to unexpected problems (y/n) "
+	while true
+	do
+		case $input in
+			[yY][eE][sS]|[yY])
+				git clone https://github.com/jlehrer1/touchid-for-sudo
+				mkdir -p "${HOME}/.touchid-for-sudo-remote-restore"
+				cp /etc/pam.d/sudo "${HOME}/.touchid-for-sudo-remote-restore"
+				cp touchid-for-sudo/.backups/sudo /etc/pam.d
+				rm -rf touchid-for-sudo/
+				echo "Successfully restored to remote backup"
+				break
+				;;
+			[nN][oO]|[nN])
+				break
+				;;
+			*)
+			echo "Invalid"
+			;;
+		esac
+	done		
 else
 	while true 
 	do
