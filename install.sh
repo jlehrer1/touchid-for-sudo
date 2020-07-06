815df63da014a7567c7943910bd35f14b2e426d8
Julian Lehrer <jmlehrer@ucsc.edu>
2020-07-06 08:07:18 -0700

    Finished install and uninstall files.. tested and works ok

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 echo "Installing..."
 mkdir -p ${HOME}/.touchid-for-sudo
-sudo cp /etc/pam.d/sudo ${HOME}/.touchid-for-sudo
-cat /etc/pam.d/sudo | sudo awk 'NR==2 {print "auth       sufficient pam_tid.so"} 1' > tempfile
-sudo mv tempfile /etc/pam.d
+cp /etc/pam.d/sudo ${HOME}/.touchid-for-sudo
+cat /etc/pam.d/sudo | awk 'NR==2 {print "auth       sufficient     pam_tid.so"} 1' > tempfile
+mv tempfile "sudo"
+mv "sudo" /etc/pam.d
 echo "Finished installing, to reset to the original state run the uninstall
 script, or copy the backup file manually found in ~/.touchid-for-sudo"
--- uninstall.sh
+++ uninstall.sh
@@ -1,1 +1,4 @@
 echo "Uninstalling..."
+cp "${HOME}/.touchid-for-sudo/sudo" /etc/pam.d/sudo
+echo "Finished uninstalling. A copy of the original /etc/pam.d/sudo file has
+been left in ~/.touchid-for-sudo"
