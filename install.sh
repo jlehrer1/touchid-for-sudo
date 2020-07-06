409d0bb97a70f26107806bbefc27a64935e4537a
Julian Lehrer <jmlehrer@ucsc.edu>
2020-07-06 08:37:12 -0700

    Updated uninstall script to be more flexible: TODO: Add copying sudo file from remote repo

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 echo "Installing..."
-mkdir -p ${HOME}/.touchid-for-sudo
-cp /etc/pam.d/sudo ${HOME}/.touchid-for-sudo
+mkdir -p "${HOME}/.touchid-for-sudo"
+cp /etc/pam.d/sudo "${HOME}/.touchid-for-sudo"
 cat /etc/pam.d/sudo | awk 'NR==2 {print "auth       sufficient     pam_tid.so"} 1' > tempfile
 mv tempfile "sudo"
 mv "sudo" /etc/pam.d
--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,26 @@
-echo "Uninstalling..."
-cp "${HOME}/.touchid-for-sudo/sudo" /etc/pam.d/sudo
-echo "Finished uninstalling. A copy of the original /etc/pam.d/sudo file has
-been left in ~/.touchid-for-sudo"
+if [ ! -f "${HOME}/.touchid-for-sudo/sudo" ]; then 
+	echo "The ~/.touchid-for-sudo folder has been deleted, and the original
+/etc/pam.d/sudo file has been lost. Would you like to restore from the remote
+file found on https://github.com/jlehrer1/touchid-for-sudo? Note that it may not
+be the same and could lead to problems. (y/n) "
+	
+else
+	while true 
+	do
+		echo "Uninstalling..."
+		cp "${HOME}/.touchid-for-sudo/sudo" /etc/pam.d/sudo
+		read -r -p "Finished uninstalling. Would you like to keep the backup found at ~/.touchid-for-sudo/sudo? (y/n) " input
+		case $input in
+			[yY][eE][sS]|[yY])
+				rm -rf "${HOME}/.touchid-for-sudo"
+				break
+				;;
+			[nN][oO]|[nN])
+				break
+				;;
+			*)
+			echo "Invalid"
+			;;
+		esac
+	done
+fi
